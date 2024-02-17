5cf694850792d75ccc3cd7a3a55d42435c714765
Akansha Kardam <kardam.akansha@gmail.com>
2024-02-18 00:51:56 +0530

    added reports for the test cases

--- TestCases/test_Login.py
+++ TestCases/test_Login.py
@@ -1,5 +1,6 @@
 import time
 from Configration.conftest import json_data
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 from PageObjects.HomePageObject import HomePageObject
@@ -10,7 +11,10 @@ from PageObjects.LoginPageObject import LoginPageObject
 
 
 class Test_Login:
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    extension_path = "Driver/adblock.crx"
+    chrome_options.add_extension(extension_path)
+    driver = webdriver.Chrome(options=chrome_options)
     baseURl = "https://automationexercise.com"
     driver.maximize_window()
     homePO = HomePageObject(driver)
@@ -19,6 +23,8 @@ class Test_Login:
 
     def test_001_signUpNewUser(self, json_data):
         self.driver.get(self.baseURl)
+        time.sleep(2)
+        self.driver.switch_to.window(self.driver.window_handles[0])
         self.homePO.navigateToSighUp_LoginPage()
         time.sleep(2)
         user_date = json_data['UserData']
@@ -29,6 +35,8 @@ class Test_Login:
 
     def test_002_loginWithCorrectUserNamePassword(self, json_data):
         self.driver.get(self.baseURl)
+        time.sleep(2)
+        self.driver.switch_to.window(self.driver.window_handles[0])
         self.homePO.navigateToSighUp_LoginPage()
         time.sleep(2)
         correct_data = json_data['CorrectUserCredentials']
